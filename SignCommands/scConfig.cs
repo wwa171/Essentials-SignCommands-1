ecda73fb5e287eddd9d94638cec6040105db4c8a
stealownz <stealownz@gmail.com>
2015-08-07 02:23:04 +0800

    Reformmatted SignCommands

--- SignCommands/scConfig.cs
+++ SignCommands/scConfig.cs
@@ -2,25 +2,22 @@
 using System.IO;
 using Newtonsoft.Json;
 
-namespace SignCommands
-{
-	public class ScConfig
-	{
-		public string DefineSignCommands = "[Sign Command]";
-        public string CommandsStartWith = ">";
-        public Dictionary<string, int> CooldownGroups = new Dictionary<string, int>();
-		public bool ShowDestroyMessage = true;
+namespace SignCommands {
+  public class ScConfig {
+    public string DefineSignCommands = "[SC]";
+    public string CommandsStartWith = ">";
+    public string FlagsStartWith = "*";
+    public Dictionary<string, int> CooldownGroups = new Dictionary<string, int>();
+    public bool ShowDestroyMessage = true;
 
-        public void Write(string path)
-        {
-            File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
-        }
+    public void Write(string path) {
+      File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
+    }
 
-        public static ScConfig Read(string path)
-        {
-            return !File.Exists(path)
-                ? new ScConfig()
-                : JsonConvert.DeserializeObject<ScConfig>(File.ReadAllText(path));
-        }
-	}
+    public static ScConfig Read(string path) {
+      return !File.Exists(path)
+          ? new ScConfig()
+          : JsonConvert.DeserializeObject<ScConfig>(File.ReadAllText(path));
+    }
+  }
 }
